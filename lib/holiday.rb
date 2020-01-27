71a3b1d565170d6457ff7bbe56c5b752c9a101af
Ian Marrs <ian.t.marrs@gmail.com>
2020-01-27 20:47:08 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -37,7 +37,7 @@ def add_supply_to_memorial_day(holiday_hash, supply)
   # add the second argument to the memorial day array
   
 end
-add_supply_to_memorial_day(holiday_supplies, "baloons")
+
 binding.pry
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
