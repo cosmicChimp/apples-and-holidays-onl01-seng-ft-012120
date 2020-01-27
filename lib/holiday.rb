0babf863a2ed14054570d3db01edd8b5d0513ffe
Ian Marrs <ian.t.marrs@gmail.com>
2020-01-27 22:04:18 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -54,7 +54,7 @@ end
 
 def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, data|
-    puts " #{season.to_s.capitalize!}:"
+    puts "#{season.to_s.capitalize!}:"
     data.each do |holiday, supply|
       array = holiday.to_s.split("_")
       final_holiday = []
