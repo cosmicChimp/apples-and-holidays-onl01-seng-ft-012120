2536a2fce1834ccd552b65e8fca2259f1e2231f9
Ian Marrs <ian.t.marrs@gmail.com>
2020-01-27 21:21:35 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -50,15 +50,12 @@ def all_winter_holiday_supplies(holiday_hash)
   holiday_hash[:winter].values.flatten
   # return an array of all of the supplies that are used in the winter season
 
-end
-require 'pry'
 def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
-    holiday.each do |holiday, item|
-      puts " #{holiday.to_s.split("_").map {|x| x.capitalize}.join(" ")}: #{item.join(", ")}"
-      binding.pry
-    end
+    holiday.each do |holiday, supplies|
+    puts "  #{holiday.to_s.split("_").map{|x| x.capitalize}.join(" ")}: #{supplies.join(", ")}"
+  end
   end
   
 
