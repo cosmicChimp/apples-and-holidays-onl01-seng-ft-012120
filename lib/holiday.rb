f983b11a0c70408030a4074cf11c96b8752cd075
Ian Marrs <ian.t.marrs@gmail.com>
2020-01-27 21:36:16 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -54,9 +54,9 @@ end
 
 def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, data|
-    "#{season.capitalize}:"
+    "#{season}:"
     data.each do |holiday, item|
-       " #{holiday.to_s.split("_").map {|x| x.capitalize}.join(" ")}: #{item.join(", ")}"
+       "#{holiday.to_s.split("_").map {|x| x}.join(" ")}: #{item.join(", ")}"
     end
     binding.pry
   end
