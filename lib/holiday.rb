b403bf4026c239ca57551bf8bf2e4c10869f1dbb
Ian Marrs <ian.t.marrs@gmail.com>
2020-01-27 21:17:08 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -54,9 +54,9 @@ end
 require 'pry'
 def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
-    puts "#{season.capitalize}:";
-    holiday.each do |holiday, item|
-      puts " #{holiday.split("_").collect {|i| i.capitalize}.join(" ")}: #{item.join(", ")}"
+    puts "#{season.capitalize}:"
+    holiday.each do |holiday, supplies|
+      puts "#{holiday.split("_").collect {|i| i.capitalize}.join(" ")}: #{item.join(", ")}"
       binding.pry
     end
     
