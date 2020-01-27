f2d06e1be13c1264b67f5e5a6b19945ebad8bab1
Ian Marrs <ian.t.marrs@gmail.com>
2020-01-27 21:45:21 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,6 +59,7 @@ def all_supplies_in_holidays(holiday_hash)
       supplies = supplies.join(", ")
       holiday = holiday.to_s.split("_").map {|w| w.capitalize}.join(" ")<< ":"
       puts "  " + holiday + " " + supplies
+      end
     end
   end
   
